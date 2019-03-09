9f3192e3742ad5043e07a8af4b0da5c296a3da2d
SrivastavaAditya <aditya.srivastava@svam.com>
2019-03-09 21:28:56 +0530

    added difference function

--- 04_functions/main.go
+++ 04_functions/main.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println(greeting("Aditya"))
 	fmt.Println(getSum(3, 6))
+	fmt.Println(getDifference(23, 987))
 }
 
 func greeting(name string) string {
@@ -13,4 +14,12 @@ func greeting(name string) string {
 
 func getSum(num1 int, num2 int) int {
 	return num1 + num2
+}
+
+func getDifference(num1 int, num2 int) int{
+	if num1>=num2{
+		return num1-num2
+	}else{
+		return num2-num1
+	}
 }
